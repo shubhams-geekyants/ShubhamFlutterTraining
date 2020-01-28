71ed72b63e8c58b1a1c6f122d83354657e5b7d64
Shubham Srivastava <shubhams@geekyants.com>
2020-01-28 11:22:32 +0530

    day 4 assignment init

---
+++ day_4_assignment/bin/main.dart
@@ -0,0 +1,8 @@
+import 'package:day_4_assignment/day_4_assignment.dart' as day_4_assignment;
+import 'dart:io';
+
+void main(List<String> arguments) {
+  stdout.write('test');
+  stdout.write('test');
+  stdout.close();
+}
---
+++ day_4_assignment/test/day_4_assignment_test.dart
@@ -0,0 +1,8 @@
+import 'package:day_4_assignment/day_4_assignment.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
